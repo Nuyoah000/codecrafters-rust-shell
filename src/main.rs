f7c1123dbf4bd7d74146bf979b427c3f9dc3e1bd
Zrey <15572669366@163.com>
2026-03-14 12:28:57 +0800

    back

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
-    print!("$ ");
-    print!("$ ");
     io::stdout().flush().unwrap();
 }
 
